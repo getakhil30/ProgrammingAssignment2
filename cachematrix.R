c7f7864d1963cad2e150c157b6b6063f5b0a1a44
getakhil30 <getakhil30@gmail.com>
2017-05-19 09:57:29 +0530

    Submitting the assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## To cache the inverse of a given matrx, we will have to assign the function call to makeCacheMatrix 
+##into a Variable. Example myInverse<-makeCacheMatrix(myMatrix)
 
-## Write a short comment describing this function
+## makeCacheMatrix function will take a matrix as its argument and will return a list of four functions.
+##This matrix will store the value for inverse of a matrix in a seperate environment 
+##and this cached value will be available via getInverse function.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv<-NULL
+        get<-function() x
+        
+        set<-function(y){
+                x<<-y
+                inv<-NULL
+        }
+        
+        getInverse<-function()inv
+        
+        setInverse<-function(inverse){
+                inv<<-inverse
+        }
+        
+        list(get=get,set=set,getInverse=getInverse,setInverse=setInverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function takes makeCacheMatrix() as its argument.
+##This function will calculate the inverse of the given Matrix if it is invertible
+##else it will return an error "Please provide a non singular Matrix"
+##If we provide a new Matrix it will calculate the inverse else it will get the value from Cache.
 
 cacheSolve <- function(x, ...) {
+        
+        inv<-x$getInverse()
+        if(!is.null(inv)){
+                message("getting data from cache")
+                inv
+        }
+        data<-x$get()
+        if((class(try(solve(data),silent=T)))!="matrix"){
+                stop("Please provide a non singular Matrix")
+        }
+        inv<-solve(data)
+        x$setInverse(inv)
+        inv
+        
         ## Return a matrix that is the inverse of 'x'
 }
